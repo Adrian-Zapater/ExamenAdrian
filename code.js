0de8abd4e4a0497f7fccd2f505656f99bf50155a
adria <adrianzu14@gmail.com>
2023-11-17 09:00:32 +0100

    9:00

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const SWAPI_BASE_URL = 'https://wizard-world-api.herokuapp.com'
+const WIZARD_BASE_URL = 'https://wizard-world-api.herokuapp.com'
 
 window.onload = async () => {
 
@@ -7,13 +7,16 @@ window.onload = async () => {
   for (const wizard of wizards){
     const mainHtmlElement = document.getElementById('wizards');
     const newElement = document.createElement('div');
-        newElement.innerHTML = ``;
+    newElement.innerHTML = `
+    <h3>${wizard.firstName}</h3>
+    `;
+    mainHtmlElement.appendChild(newElement);
   }
 
 }
 
 async function getAllWizards(){
-  const response = await fetch(`${SWAPI_BASE_URL}/Wizards`);
-  const data = await responde.json();
-  return data.firstName;
+  const response = await fetch(`${WIZARD_BASE_URL}/Wizards`);
+  const data = await response.json();
+  return data.results;
 }
