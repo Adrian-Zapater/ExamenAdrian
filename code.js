ed6b8835a8caad418a8173b88e547a6fae37c866
adria <adrianzu14@gmail.com>
2023-11-20 00:53:45 +0100

    minims complerts (vaig estar tot el fin de semana fora y no vaig puger fer res fins dumenge nit)

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ window.onload = async () => {
     const newElement = document.createElement('div');
 
     newElement.innerHTML = `<h1>${wizard.firstName}</h1>
-    <button onclick="destacarPersonaje('${wizard.firstName}')" id="destacarPersonaje">Destacar</button>
+    <button onclick="destacarPersonaje('${wizard.id}')" id="destacarPersonaje">Destacar</button>
     <p></p>`;
 
     if (wizard.firstName !== null){
@@ -31,7 +31,7 @@ window.onload = async () => {
     const mainHtmlElement2 = document.getElementById('houses');
     const newElement2 = document.createElement('div');
 
-    newElement2.innerHTML = `<div class="escudos ${house.name}">${house.name}<button onclick="destacarCasa('${house.name}')" id="destacarCasa">Destacar</button></div>`;
+    newElement2.innerHTML = `<div class="escudos ${house.name}">${house.name}<button onclick="destacarCasa('${house.id}')" id="destacarCasa">Destacar</button></div>`;
     mainHtmlElement2.appendChild(newElement2);
   }
 }
@@ -54,7 +54,21 @@ async function getIngredientsBysElixirs(elixirId){
   return data.ingredients;
 }
 
+async function getWizardById(WizardId){
+  const response = await fetch(`${WIZARD_BASE_URL}/Wizards/${WizardId}`);
+  const data = await response.json();
+  return data;
+}
+
+async function getHouseById(HouseId){
+  const response = await fetch(`${WIZARD_BASE_URL}/Houses/${HouseId}`);
+  const data = await response.json();
+  return data;
+}
+
 async function showIngredients (elixirId){
+  cerrarIngredients();
+  document.getElementById('ingredients').style.display = 'block';
   const sectionHtmlElement = document.getElementById('ingredients');
   sectionHtmlElement.innerHTML = '';
   const ingredients = await getIngredientsBysElixirs(elixirId);
@@ -65,10 +79,56 @@ async function showIngredients (elixirId){
     newElement.innerHTML = `<p>${ingredient.name}</p>`;
     sectionHtmlElement.appendChild(newElement);
   }
-  newElement.innerHTML = `<button class="botonIngredients" onclick="cerrarIngredientsl()">Cerrar</button>`
-  sectionHtmlElement.appendChild(newElement);
+  const newElement2 = document.createElement('div');
 }
 
 function cerrarIngredients() {
-  document.getElementById('ingredientes').style.display = 'none';
+  document.getElementById('ingredients').style.display = 'none';
+}
+
+function cerrarWizard() {
+  document.getElementById('wizard').style.display = 'none';
+}
+
+function cerrarHouse() {
+  document.getElementById('house').style.display = 'none';
+}
+
+async function destacarPersonaje(wizardId) {
+  cerrarIngredients();
+  cerrarWizard();
+  document.getElementById('wizard').style.display = 'block';
+  const sectionHtmlElement = document.getElementById('wizard');
+  sectionHtmlElement.innerHTML = '';
+  const wizard = await getWizardById(wizardId);
+
+  const newElement = document.createElement('div');
+  newElement.innerHTML = `
+  <h1>${wizard.firstName}</h1>
+  <p>${wizard.lastName}</p>
+  <button class="botonWizard" onclick="cerrarWizard()">Cerrar</button>`;
+
+  sectionHtmlElement.appendChild(newElement);
+}
+
+async function destacarCasa(houseId) {
+  cerrarIngredients();
+  cerrarHouse();
+  document.getElementById('house').style.display = 'block';
+  const sectionHtmlElement = document.getElementById('house');
+  sectionHtmlElement.innerHTML = '';
+  const house = await getHouseById(houseId);
+
+  const newElement = document.createElement('div');
+  newElement.innerHTML = `
+  <h1>${house.name}</h1>
+  <p>Colours: ${house.houseColours}</p>
+  <p>Founder: ${house.founder}</p>
+  <p>Animal: ${house.animal}</p>
+  <p>Element: ${house.element}</p>
+  <p>Ghost: ${house.ghost}</p>
+  <p>Common Room: ${house.commonRoom}</p>
+  <button class="botonHouse" onclick="cerrarHouse()">Cerrar</button>`;
+
+  sectionHtmlElement.appendChild(newElement);
 }
